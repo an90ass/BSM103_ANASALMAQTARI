cfc43cd395c2342e17f48838504d57f990e63888
an90ass <21670310176@ogrenci.bartin.edu.tr>
2021-12-31 00:55:53 +0300

    Bir dizi tum elemenlerini Javadaki baska bir diziye kopyalam
    Diziler

--- DENEMELER/src/DENEMELER/Anas_Almaqtari_Denemeler14.java
+++ DENEMELER/src/DENEMELER/Anas_Almaqtari_Denemeler14.java
@@ -8,15 +8,14 @@ public class Anas_Almaqtari_Denemeler14 {
 
         System.out.print(" Dizi Boyutunu Girin :");
        int DiziBoyutunu = sc.nextInt();
+       
         int[] BirinciDizi = new int[DiziBoyutunu];
         int[] IkinciDizi= new int[DiziBoyutunu];
 
-        System.out.print("Baska bir diziye kopyalama istediginiz deger giriniz :");
 
-        for (int index = 0; index < BirinciDizi.length; index++) {
-        	BirinciDizi[index] = sc.nextInt();
-        	
-            System.out.print(" Baska bir diziye kopyalama istediginiz rakamlar giriniz :");
+        for (int index = 0; index < BirinciDizi.length ; index++) {
+         System.out.print(" Baska bir diziye kopyalama istediginiz rakamlar giriniz :");
+     	BirinciDizi[index] = sc.nextInt();
 
         }
  
