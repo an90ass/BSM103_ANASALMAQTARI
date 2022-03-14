bb839cd0b796c18c7c8a74090843ecd220eb1128
an90ass <21670310176@ogrenci.bartin.edu.tr>
2022-03-14 22:53:18 +0300

    week2
    week2

---
+++ Weekk2/src/ikincidonem/Anas_Almaqtari_week2_ornek1.java
@@ -0,0 +1,24 @@
+package ikincidonem;
+
+import java.util.Iterator;
+
+public class Anas_Almaqtari_week2_ornek1 {
+
+	public static void main(String[] args) {
+	int[] array = {3,2,8,9,10};
+	isUnique(array);
+	
+	}
+	public static boolean isUnique(int[] array) {
+		for ( int sayac = 0; sayac<array.length ; sayac++) {
+			for (int sutun = sayac + 1; sutun < array.length; sutun++) {
+				if (array[sayac]!= array[sutun]) {
+					return true;
+			}
+			}
+		}
+	return false;
+	}}
+			
+
+
