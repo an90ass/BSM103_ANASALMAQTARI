14acc567ef9a7b214d9ed308e88884e32e868e10
an90ass <21670310176@ogrenci.bartin.edu.tr>
2022-03-19 01:25:39 +0300

    Update Stock.java

--- Week4donem2/src/ikincidonem/Stock.java
+++ Week4donem2/src/ikincidonem/Stock.java
@@ -3,29 +3,29 @@ package ikincidonem;
 public class Stock {
                 
 	private String sotre; 
-	private int totalProduct;
+	private int toplamUrun;
 	private int totalCost;
 	public Stock(String thestore) {
 		if (thestore==null) {
 			throw new NullPointerException();
              }
 		sotre = thestore;
-		totalProduct= 0;
+		toplamUrun= 0;
 		totalCost= 0;
 	    }
-	public int getProfit(int currentPrice) {
-	       if (currentPrice < 0 ) {
+	public int getProfit(int Mevcutfiyat) {
+	       if (Mevcutfiyat < 0 ) {
 	    	   throw new IllegalArgumentException();   
 	       }
-	       int marketValue = totalProduct * currentPrice;
-	       return marketValue - totalCost;
+	       int marketdegeri  = toplamUrun * Mevcutfiyat;
+	       return marketdegeri - totalCost;
 	}
 	
-	public void purchase(int shares, int pricePerShare) {
+	public void satinalma(int shares, int pricePerShare) {
 		if (shares < 0 || pricePerShare<0) {
 	    	   throw new IllegalArgumentException();   
              }
-		totalProduct +=shares;
+		toplamUrun +=shares;
 		totalCost +=shares * pricePerShare;
 			
 	}
