aabd0dc6fb5e3c9c07f0227eb1830cfde0fe873c
an90ass <21670310176@ogrenci.bartin.edu.tr>
2022-06-03 20:45:08 +0300

    HAFTA15 DONEM2_Functional Programming (Fonsiyonel Programlama)
    HAFTA15 DONEM2_Functional Programming (Fonsiyonel Programlama)

---
+++ Week15donem2/src/ikincidonem/MathTutor.java
@@ -0,0 +1,28 @@
+package ikincidonem;
+import java.util.*;
+import java.util.function.IntBinaryOperator;
+public class MathTutor {
+	public static void main(String[] args) {
+      Scanner console = new Scanner(System.in);
+       giveProblems(console , 3 , "*" , (x ,y ) -> x *y);
+       
+	}
+	public static void giveProblems(Scanner console , int numProblems , String text , IntBinaryOperator operator) {
+		Random r = new Random();
+		int numRight = 0;
+		for (int i = 0; i < numProblems; i++) {
+			int x = r.nextInt(12)+1;
+			int y = r.nextInt(12)+1;
+			System.out.println(x + " " + text + " " +y+ " = " );
+			int answer = operator.applyAsInt(x,y);
+			int response = console.nextInt();
+			if(response == answer) {
+				System.out.println("Dogru");
+				numRight ++;
+			}else
+			System.out.println("Yanlis cevap:" + answer);
+		}
+		System.out.println(numProblems+ "Cevap Dogru Sayisi : " +numRight);		
+		}
+	}
+
